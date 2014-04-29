016726c953f1b2462f94e5cc1a2be37385232e51
patrickshampine <patrick@patrickshampine.com>
2014-04-28 23:43:40 -0700

    Updates

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 gitpress
 =======
 
-### License
+### usage
+
+Install as a plugin or as a submodule.
+
+Use the function to call the plugin in a theme.
+
+```
+gitpress();
+```
+
+Classes to override `.gitpress-container`, `.gitpress-active` and `.gitpress`. Defaults to pos:abs, bot:0, width:100%.
+
+### todo
+  - shortcode
+  - less intense styles, or none
+  - fix fadeIn function
+
+### license
 
 MIT
--- gitpress.php
+++ gitpress.php
@@ -9,90 +9,99 @@ Author URI: http://patrickshampine.com
 Author Email: patrick@patrickshampine.com
 */
 
-$rss = fetch_feed('https://github.com/patrickshampine.atom');
+function gitpress() {
+  $rss = fetch_feed('https://github.com/patrickshampine.atom');
 
-if (!is_wp_error($rss)) {
-  $maxitems = $rss->get_item_quantity(10);
-  $rss_items = $rss->get_items(0, $maxitems);
-}
-
-$count = 0;
-
-echo '<div class="gitrss-container">';
-
-foreach($rss_items as $history) {
-
-  $data = $history->data;
-  $content = $data['child']['http://www.w3.org/2005/Atom'];
-  $published = $content['published'][0]['data'];
-  $link = $content['link'][0]['attribs']['']['href'];
-  $title = $content['title'][0]['data'];
-
-  $active = '';
-
-  if($count === 0) {
-    $active = 'gitrss-active';
+  if (!is_wp_error($rss)) {
+    $maxitems = $rss->get_item_quantity(10);
+    $rss_items = $rss->get_items(0, $maxitems);
   }
 
-  echo '<div class="gitrss '.$active.'" data-gitrss="'.$count.'" target="_blank">';
-  echo '<h6><a href="'.$link.'">'.$title.'</a></h6>';
+  $count = 0;
+
+  echo '<div class="gitrss-container">';
+
+  foreach($rss_items as $history) {
+
+    $data = $history->data;
+    $content = $data['child']['http://www.w3.org/2005/Atom'];
+    $published = $content['published'][0]['data'];
+    $link = $content['link'][0]['attribs']['']['href'];
+    $title = $content['title'][0]['data'];
+
+    $active = '';
+
+    if($count === 0) {
+      $active = 'gitrss-active';
+    }
+
+    echo '<div class="gitrss '.$active.'" data-gitrss="'.$count.'" target="_blank">';
+    echo '<h6><a href="'.$link.'">'.$title.'</a></h6>';
+    echo '</div>';
+
+    $count++;
+
+    if($count >= 10) {
+      break;
+    }
+  }
+
   echo '</div>';
-
-  $count++;
-
-  if($count >= 10) {
-    break;
-  }
 }
-
-echo '</div>';
-
 
 function gitpressScripts() { ?>
-
   <style>
     .gitrss-container {
-      position: relative;
+      position: absolute;
       overflow: hidden;
       width: 100%;
       height: 50px;
+      bottom: 0;
+      background-color: lightgray;
     }
     .gitrss {
       display:none;
-      position: absolute;
+      position: relative;
+      width: 100%;
+      height: inherit;
       margin: 0 auto;
       text-align: center;
-      background-color: lightgray;
+    }
+    .gitrss h6 {
+      padding: 10px;
+      font-size: 1.1em;
     }
     .gitrss-active {
       display: block;
     }
   </style>
   <script>
-  var gitrss = $('.gitrss');
+  jQuery(document).ready(function($) {
+    var gitrss = $('.gitrss');
 
-  function fade() {
-      var current = $('.gitrss-active');
-      var currentIndex = gitrss.index(current),
-          nextIndex = currentIndex + 1;
-      
-      if (nextIndex >= gitrss.length) {
-          nextIndex = 0;
-      }
-      
-      var next = gitrss.eq(nextIndex);
-      
-      next.stop().fadeIn(2500, function() {
-          $(this).addClass('gitrss-active');
-      });
-      
-      current.stop().fadeOut(2500, function() {
-          $(this).removeClass('gitrss-active');
-          setTimeout(fade, 3500);
-      });
-  }
+    function fade() {
+        var current = $('.gitrss-active');
+        var currentIndex = gitrss.index(current),
+            nextIndex = currentIndex + 1;
+        
+        if (nextIndex >= gitrss.length) {
+            nextIndex = 0;
+        }
+        
+        var next = gitrss.eq(nextIndex);
+        
+        next.stop().fadeIn(1500, function() {
+            $(this).addClass('gitrss-active');
+        });
+        
+        current.stop().fadeOut(2500, function() {
+            $(this).removeClass('gitrss-active');
+            setTimeout(fade, 2500);
+        });
+    }
 
-  fade();
+    fade();
+  });
   </script><?php
 
 }
